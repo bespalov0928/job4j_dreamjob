b7c69d8cc88f9cbeda4848059d04e00bfbaaa471
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2021-05-15 18:31:18 +0300

    3/2/8/4.1. JSON формат. [#444658]

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,5 +1,8 @@
 package ru.job4j.dream.servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,15 +11,38 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class GreetingServlet extends HttpServlet {
+
+    private static final  Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain");
         resp.setCharacterEncoding("UTF-8");
         String name = req.getParameter("name");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-//        writer.println("Nice to meet you, "+name);
-        writer.println(name);
+
+        Data data = new Data(name);
+        String json = GSON.toJson(data);
+        System.out.println(json);
+        //writer.println("Nice to meet you, "+name);
+        //writer.println(name);
+        writer.println(json);
         writer.flush();
-        //req.getRequestDispatcher("ajax.html").forward(req, resp);
+    }
+}
+
+class Data{
+    private String text;
+
+    public Data(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return text;
+    }
+
+    public void setName(String text) {
+        this.text = text;
     }
 }
